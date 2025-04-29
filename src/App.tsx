62d99c818fdbc2ac2d4b4235eb7d2948af611d0b
Seono-Na <seono5670@gmail.com>
2025-04-29 21:25:04 +0900

    chore: react query QueryClientProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { IssueListPage } from './pages/IssueListPage';
 
 export function App() {
-  return <IssueListPage />;
+  const queryClient = new QueryClient();
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <IssueListPage />;
+      </QueryClientProvider>
+      ;
+    </>
+  );
 }
